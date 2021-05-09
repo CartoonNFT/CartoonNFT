521ebc5f7b08fec6779c8b2655881eabffd48884
nanashi <nanashichef@outlook.com>
2021-05-09 13:38:16 +0800

    build(deploy): update deploy setting

--- deploy/Setting.ts
+++ deploy/Setting.ts
@@ -19,100 +19,88 @@ module.exports = async ({ ethers, getNamedAccounts, deployments, getChainId, get
   const coinWindPool = await ethers.getContract("CoinWindPool")
   const coinWindPoolNFT = await ethers.getContract("CoinWindPoolWithNFT")
   const masterChef = await ethers.getContract("MasterChef")
-  // const teamTimeLock = await ethers.getContract("TeamTimeLock")
-  // const foundationTimeLock = await ethers.getContract("FoundationTimeLock")
-
-  // timelock
-  // await cto.connect(dev).grantRole(await cto.MINT_ROLE(), deployer)
-  // const lockReward = (await cto.cap()).div(10).mul(99).div(100)
-  // await cto.mint(teamTimeLock.address, lockReward)
-  // await cto.mint(foundationTimeLock.address, lockReward)
-
-  // //air drop
-  // const airDropReward = (await cto.cap()).mul(2).div(100)
-  // await cto.mint(airDrop, airDropReward)
-
-  // //consultant
-  // const consultantReward = (await cto.cap()).mul(2).div(100)
-  // await cto.mint(consultant, consultantReward)
-  // //cooperations
-  // const cooperationsReward = (await cto.cap()).mul(7).div(100)
-  // await cto.mint(cooperations, cooperationsReward)
-  // //operations
-  // const operationsReward = (await cto.cap()).mul(6).div(100)
-  // await cto.mint(operations, operationsReward)
-  // //IDO
-  // const IDOReward = (await cto.cap()).mul(13).div(100)
-  // await cto.mint(IDO, IDOReward)
-
-  // await cto.connect(dev).revokeRole(await cto.MINT_ROLE(), deployer)
 
   // add card types
-  await cardSpec.addCardType(1000, 1000, ethers.utils.formatBytes32String("role1"))
-  await cardSpec.addCardType(1001, 1000, ethers.utils.formatBytes32String("role2"))
-  await cardSpec.addCardType(1002, 1000, ethers.utils.formatBytes32String("role3"))
-  await cardSpec.addCardType(1003, 1000, ethers.utils.formatBytes32String("role4"))
-  await cardSpec.addCardType(1004, 1000, ethers.utils.formatBytes32String("role5"))
+  await cardSpec.addCardType(1000, 1001, ethers.utils.formatBytes32String("Jun Uozumi"))
+  await cardSpec.addCardType(1001, 1002, ethers.utils.formatBytes32String("Akira Sendoh"))
+  await cardSpec.addCardType(1002, 1000, ethers.utils.formatBytes32String("Kicchou Fukuda"))
+  await cardSpec.addCardType(1003, 1000, ethers.utils.formatBytes32String("Hiroaki Koshino"))
+  await cardSpec.addCardType(1004, 1000, ethers.utils.formatBytes32String("Tomoyuki Uekusa"))
 
-  await cardSpec.addCardType(1005, 1000, ethers.utils.formatBytes32String("role6"))
-  await cardSpec.addCardType(1006, 1000, ethers.utils.formatBytes32String("role7"))
-  await cardSpec.addCardType(1007, 1000, ethers.utils.formatBytes32String("role8"))
-  await cardSpec.addCardType(1008, 1000, ethers.utils.formatBytes32String("role9"))
-  await cardSpec.addCardType(1009, 1000, ethers.utils.formatBytes32String("role10"))
+  await cardSpec.addCardType(1005, 1002, ethers.utils.formatBytes32String("Hanamichi Sakuragi"))
+  await cardSpec.addCardType(1006, 1001, ethers.utils.formatBytes32String("Takenori Akagi"))
+  await cardSpec.addCardType(1007, 1002, ethers.utils.formatBytes32String("Kaede Rukawa"))
+  await cardSpec.addCardType(1008, 1001, ethers.utils.formatBytes32String("Hisashi Mitsui"))
+  await cardSpec.addCardType(1009, 1000, ethers.utils.formatBytes32String("Ryota Miyagi"))
 
-  await cardSpec.addCardType(1010, 1000, ethers.utils.formatBytes32String("role11"))
-  await cardSpec.addCardType(1011, 1000, ethers.utils.formatBytes32String("role12"))
-  await cardSpec.addCardType(1012, 1000, ethers.utils.formatBytes32String("role13"))
-  await cardSpec.addCardType(1013, 1000, ethers.utils.formatBytes32String("role14"))
-  await cardSpec.addCardType(1014, 1000, ethers.utils.formatBytes32String("role15"))
+  await cardSpec.addCardType(1010, 1002, ethers.utils.formatBytes32String("Shinichi Maki"))
+  await cardSpec.addCardType(1011, 1001, ethers.utils.formatBytes32String("Soichiro Jin"))
+  await cardSpec.addCardType(1012, 1001, ethers.utils.formatBytes32String("Nobunaga Kiyota"))
+  await cardSpec.addCardType(1013, 1000, ethers.utils.formatBytes32String("Yoshinori Miyawasu"))
+  await cardSpec.addCardType(1014, 1000, ethers.utils.formatBytes32String("Kazuma Takasago"))
 
-  await cardSpec.addCardType(2000, 2000, ethers.utils.formatBytes32String("lv1"))
-  await cardSpec.addCardType(2000, 2001, ethers.utils.formatBytes32String("lv2"))
-  await cardSpec.addCardType(2000, 2002, ethers.utils.formatBytes32String("lv3"))
+  await cardSpec.addCardType(1015, 1002, ethers.utils.formatBytes32String("Kenji Fujima"))
+  await cardSpec.addCardType(1016, 1001, ethers.utils.formatBytes32String("Toru Hanagata"))
+  await cardSpec.addCardType(1017, 1000, ethers.utils.formatBytes32String("Kazushi Hasegawa"))
+  await cardSpec.addCardType(1018, 1000, ethers.utils.formatBytes32String("Mitsuru Nagano"))
+  await cardSpec.addCardType(1019, 1000, ethers.utils.formatBytes32String("Shoichi Takano"))
+
+  await cardSpec.addCardType(2000, 1000, ethers.utils.formatBytes32String("MiningCardv1"))
+  await cardSpec.addCardType(2001, 1001, ethers.utils.formatBytes32String("MiningCardv2"))
+  await cardSpec.addCardType(2002, 1002, ethers.utils.formatBytes32String("MiningCardv3"))
 
   // mint card to shop
   await ncto.grantRole(await ncto.MINT_ROLE(), shop.address)
   await shop.setUnitPrice(ethers.utils.parseUnits("1", 18))
-  for (let i = 0; i < 15; i++) {
-    await shop.changeBlindBoxCard(i, 5)
+
+
+  for (let i = 0; i < 20; i++) {
+    await shop.changeBlindBoxCard(i, 100)
   }
 
   // swapMarket
   await ncto.grantRole(await ncto.MINT_ROLE(), shop.address)
   await swapMarket.setUnitPrice(ethers.utils.parseUnits("1", 18))
-  await swapMarket.addSwapCardList(0, [1, 2, 3, 4])
-  await swapMarket.addSwapCardList(1, [0, 2, 3, 4])
-  await swapMarket.addSwapCardList(2, [0, 1, 3, 4])
-  await swapMarket.addSwapCardList(3, [0, 1, 2, 4])
-  await swapMarket.addSwapCardList(4, [0, 1, 2, 3])
+  await swapMarket.addSwapCardList(2, [3, 4, 9, 13, 14, 17, 18, 19])
+  await swapMarket.addSwapCardList(3, [2, 4, 9, 13, 14, 17, 18, 19])
+  await swapMarket.addSwapCardList(4, [2, 3, 9, 13, 14, 17, 18, 19])
+  await swapMarket.addSwapCardList(9, [2, 3, 4, 13, 14, 17, 18, 19])
+  await swapMarket.addSwapCardList(13, [2, 3, 4, 9, 14, 17, 18, 19])
+  await swapMarket.addSwapCardList(14, [2, 3, 4, 9, 13, 17, 18, 19])
+  await swapMarket.addSwapCardList(17, [2, 3, 4, 9, 13, 14, 18, 19])
+  await swapMarket.addSwapCardList(18, [2, 3, 4, 9, 13, 14, 17, 19])
+  await swapMarket.addSwapCardList(19, [2, 3, 4, 9, 13, 14, 17, 18])
 
-  await swapMarket.addSwapCardList(5, [6, 7, 8, 9])
-  await swapMarket.addSwapCardList(6, [5, 7, 8, 9])
-  await swapMarket.addSwapCardList(7, [5, 6, 8, 9])
-  await swapMarket.addSwapCardList(8, [5, 6, 7, 9])
-  await swapMarket.addSwapCardList(9, [5, 6, 7, 8])
 
-  await swapMarket.addSwapCardList(10, [11, 12, 13, 14])
-  await swapMarket.addSwapCardList(11, [10, 12, 13, 14])
-  await swapMarket.addSwapCardList(12, [10, 11, 13, 14])
-  await swapMarket.addSwapCardList(13, [10, 11, 12, 14])
-  await swapMarket.addSwapCardList(14, [10, 11, 12, 13])
+
+  await swapMarket.addSwapCardList(0, [6, 8, 11, 12, 16])
+  await swapMarket.addSwapCardList(6, [0, 8, 11, 12, 16])
+  await swapMarket.addSwapCardList(8, [0, 6, 11, 12, 16])
+  await swapMarket.addSwapCardList(11, [0, 6, 8, 12, 16])
+  await swapMarket.addSwapCardList(12, [0, 6, 8, 12, 16])
+  await swapMarket.addSwapCardList(16, [0, 6, 8, 11, 12])
+
+  await swapMarket.addSwapCardList(1, [5, 7, 10, 15])
+  await swapMarket.addSwapCardList(5, [1, 7, 10, 15])
+  await swapMarket.addSwapCardList(7, [1, 5, 10, 15])
+  await swapMarket.addSwapCardList(10, [1, 5, 7, 15])
+  await swapMarket.addSwapCardList(15, [1, 5, 7, 10])
 
   //  set synthesisMarketV1
   await ncto.grantRole(await ncto.MINT_ROLE(), synthesisMarketV1.address)
   await synthesisMarketV1.setUnitPrice(ethers.utils.parseUnits("1", 18))
-  await synthesisMarketV1.setSynthesisCardId(15)
-  await synthesisMarketV1.addSwapCardList([0, 1, 2, 3, 4])
+  await synthesisMarketV1.setSynthesisCardId(20)
+  await synthesisMarketV1.addSwapCardList([2, 3, 4, 9, 13, 14, 17, 18, 19])
   //  set synthesisMarketV2
   await ncto.grantRole(await ncto.MINT_ROLE(), synthesisMarketV2.address)
   await synthesisMarketV2.setUnitPrice(ethers.utils.parseUnits("1", 18))
-  await synthesisMarketV2.setSynthesisCardId(16)
-  await synthesisMarketV2.addSwapCardList([5, 6, 7, 8, 9])
+  await synthesisMarketV2.setSynthesisCardId(21)
+  await synthesisMarketV2.addSwapCardList([0, 6, 8, 11, 12, 16])
   //  set synthesisMarketV3
   await ncto.grantRole(await ncto.MINT_ROLE(), synthesisMarketV3.address)
   await synthesisMarketV3.setUnitPrice(ethers.utils.parseUnits("1", 18))
-  await synthesisMarketV3.setSynthesisCardId(17)
-  await synthesisMarketV3.addSwapCardList([10, 11, 12, 13, 14])
+  await synthesisMarketV3.setSynthesisCardId(22)
+  await synthesisMarketV3.addSwapCardList([1, 5, 7, 10, 15])
 
   // coinWind
   await cto.grantRole(await cto.MINT_ROLE(), coinWindPool.address)
