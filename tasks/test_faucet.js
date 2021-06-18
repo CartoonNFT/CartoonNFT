24445f67288cf8a23a24f6b9d04c0d791da78f23
nanashi <nanashichef@outlook.com>
2021-06-18 19:03:17 +0800

    chore(faucet task): modified faucet task

--- tasks/test_faucet.js
+++ tasks/test_faucet.js
@@ -1,35 +1,32 @@
 require("@nomiclabs/hardhat-ethers");
 
 // task action function receives the Hardhat Runtime Environment as second argument
-task(
-  "faucet",
-  "send ethers and token to hardhat network",
-  async function (_, { ethers, network }) {
+task("faucet", "send ethers and token to hardhat network")
+  .addOptionalParam("account", "The account for receiving from faucet")
+  .setAction(async function (params, { ethers, network }) {
     const { USDT } = await getNamedAccounts();
     const { deployer, dev } = await getNamedAccounts();
-    const rewardAddress = process.env.TEST_REWARD_ADDRESS;
     await network.provider.request({
       method: "hardhat_impersonateAccount",
-      params: ["0x631fc1ea2270e98fbd9d92658ece0f5a269aa161"],
+      params: ["0xf977814e90da44bfa03b6295a0616a897441acec"],
     });
     const wallet = await ethers.getSigner(
-      "0x631fc1ea2270e98fbd9d92658ece0f5a269aa161"
+      "0xf977814e90da44bfa03b6295a0616a897441acec"
     );
     // Send ethers
     await wallet.sendTransaction({
-      to: rewardAddress,
+      to: params.account,
       value: ethers.utils.parseEther("1000.0"),
     });
     const USDTContract = await ethers.getContractAt("IERC20", USDT);
     await USDTContract.connect(wallet).transfer(
-      rewardAddress,
-      ethers.utils.parseEther("100000000.0")
+      params.account,
+      ethers.utils.parseEther("100000.0")
     );
 
     const cto = await ethers.getContract("CartoonToken");
     await cto.grantRole(await cto.MINT_ROLE(), deployer);
-    await cto.mint(rewardAddress, ethers.utils.parseEther("100000.0"));
-  }
-);
+    await cto.mint(params.account, ethers.utils.parseEther("100000.0"));
+  });
 
 module.exports = {};
